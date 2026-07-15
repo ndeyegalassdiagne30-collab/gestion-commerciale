31556554192a73d30e305bc5bf3677332083452d
crybaby05 <benthiam750@gmail.com>
2026-07-15 14:55:14 +0000

    Ajout de la classe ClientService pour gérer la logique métier liée aux clients.

--- src/service/ClientService.java
+++ src/service/ClientService.java
@@ -1,5 +1,34 @@
-package src.entities.service;
+package src.service;
 
+import src.entities.Client;
+import src.repository.ClientRepository;
+import java.util.List;
+
+/**
+ * Contient la logique métier liée aux clients.
+ */
 public class ClientService {
-    
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client ajouterClient(String nom, String prenom, String telephone) {
+        return clientRepository.ajouter(nom, prenom, telephone);
+    }
+
+    public List<Client> listerClients() {
+        return clientRepository.getTous();
+    }
+
+    public Client trouverParId(int id) {
+        return clientRepository.trouverParId(id);
+    }
+
+    // Bonus : recherche d'un client par téléphone
+    public List<Client> rechercherParTelephone(String telephone) {
+        return clientRepository.rechercherParTelephone(telephone);
+    }
 }
